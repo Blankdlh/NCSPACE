56beb0c5fdb433c3bec140c9186dbf62eaee72cb
Blank <dailh.blank@gmail.com>
2012-04-05 12:23:26 +0800

    add about,contact

--- main.py
+++ main.py
@@ -19,8 +19,41 @@ class MainPage(webapp.RequestHandler):
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
+			
+class AboutPage(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+
+        if user:
+			template_values = {
+				'user_name': user.nickname()
+				}
+
+			path = os.path.join(os.path.dirname(__file__), 'template/about.html')
+			self.response.out.write(template.render(path, template_values))
+            
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class ContactPage(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+
+        if user:
+			template_values = {
+				'user_name': user.nickname()
+				}
+
+			path = os.path.join(os.path.dirname(__file__), 'template/contact.html')
+			self.response.out.write(template.render(path, template_values))
+            
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+			
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/', MainPage),
+									 ('/about', AboutPage),
+									 ('/contact', ContactPage)],
                                      debug=True)
 
 def main():
